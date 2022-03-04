360938f2ba61f414dbf821a0986be7ef797ffdd1
Henry Miller <mail.henrym+git@gmail.com>
2022-03-03 22:51:12 -0500

    commit tree e3e60f14134f3392262f4c683252dd2b9260b1a7

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,34 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    messages = Message.all
+    messages.to_json
+    # Message.all.to_json
+  end
+  
+  post '/messages' do
+    msg = Message.create(
+      body: params[:body],
+      username: params[:name]
+      )
+    msg.to_json
+    # Message.create( body: params[:body], username: params[:name]).to_json
+  end
+
+  delete '/messages/:id' do
+    msg = Message.find(params[:id])
+    msg.destroy
+    msg.to_json
+  end
+
+  patch '/messages/:id' do
+    msg = Message.find(params[:id])
+    msg.update(
+      body: params[:body],
+    )
+    msg.to_json
+  end
+
   
 end
